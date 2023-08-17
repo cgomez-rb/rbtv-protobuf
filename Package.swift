99e0b94e15a129687ec733524d1cb29ffebdef91
Claudio Gomez <claudio.gomez@redbull.com>
2023-08-17 13:54:20 -0600

    Change the location of the c and cxx settings to the target.

--- Package.swift
+++ Package.swift
@@ -20,18 +20,16 @@ let package = Package(
         // .package(url: /* package url */, from: "1.0.0"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Protobuf",
             dependencies: [],
-            publicHeadersPath: "include"
+            publicHeadersPath: "include",
+            cSettings: [
+                .define("GCC_PREPROCESSOR_DEFINITIONS", to: "$(inherited) GPB_USE_PROTOBUF_FRAMEWORK_IMPORTS=1")
+            ],
+            cxxSettings: [
+                .define("GCC_PREPROCESSOR_DEFINITIONS", to: "$(inherited) GPB_USE_PROTOBUF_FRAMEWORK_IMPORTS=1")
+            ]
         )
-    ],
-    cSettings: [
-        .define("GCC_PREPROCESSOR_DEFINITIONS", to: "$(inherited) GPB_USE_PROTOBUF_FRAMEWORK_IMPORTS=1")
-    ],
-    cxxSettings: [
-        .define("GCC_PREPROCESSOR_DEFINITIONS", to: "$(inherited) GPB_USE_PROTOBUF_FRAMEWORK_IMPORTS=1")
     ]
 )
